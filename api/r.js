0a6bd86f599135da414b417751a02e087617d298
JZZJ98 <165297505+JZZJ98@users.noreply.github.com>
2026-03-04 17:20:14 +0800

    Update r.js

--- api/r.js
+++ api/r.js
@@ -1,24 +1,3 @@
-function withCors(req, res) {
-  const origin = req.headers.origin || '';
-  const allowedOrigin = 'https://JZZJ98.github.io'; 
-
-  if (origin === allowedOrigin || origin.endsWith('.github.io')) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Vary', 'Origin');
-  }
-
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-
-  if (req.method === 'OPTIONS') {
-    res.status(204).end();
-    return true;
-  }
-
-  return false;
-}
-
 export default async function handler(req, res) {
   try {
     const url = new URL(req.url, `https://${req.headers.host}`);
