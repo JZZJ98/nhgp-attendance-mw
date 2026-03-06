54cd266b05bc6b26a29e74b9aa7af81bcdbd2b52
JZZJ98 <165297505+JZZJ98@users.noreply.github.com>
2026-03-06 18:11:02 +0800

    Delete api/webhooks/_debug.js

--- api/webhooks/_debug.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  console.log('[webhooks/_debug] hit:', {
-    method: req.method,
-    hasSig: !!req.headers['x-formsg-signature'],
-    contentType: req.headers['content-type'] || '(none)'
-  })
-  return res.status(200).json({ ok: true })
-}
